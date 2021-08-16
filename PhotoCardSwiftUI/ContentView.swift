64f092c3665cc524b81d19ef9c8baf3dd78220c7
Allicia Viona <alliciawn@gmail.com>
2021-08-16 09:38:40 +0700

    Initial Commit

---
+++ PhotoCardSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoCardSwiftUI
+//
+//  Created by Allicia Viona Sagi on 16/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
